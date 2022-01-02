c27cd66468e34af3ebc603eb61f34a25e0616a04
HSKINK <yuhikwut@gmail.com>
2022-01-02 23:21:50 +0900

    show

--- app/controllers/fats_controller.rb
+++ app/controllers/fats_controller.rb
@@ -1,17 +1,15 @@
 class FatsController < ApplicationController
 
   def index
-    if user_signed_in?
-      @fats = current_user.fats.order('day DESC')
-      @bodies = Body.all
+    @fats = current_user.fats.order('day DESC')
+    @bodies = Body.all
 
-      weights = current_user.fats.group_by_day(:day, series: false).sum(:weight)
-      body_fats = current_user.fats.group_by_day(:day, series: false).sum(:body_fat)
-      @chart = [
-        { name: "体重", data: weights },
-        { name: "体脂肪", data: body_fats }
-      ]
-    end
+    weights = current_user.fats.group_by_day(:day, series: false).sum(:weight)
+    body_fats = current_user.fats.group_by_day(:day, series: false).sum(:body_fat)
+    @chart = [
+      { name: "体重", data: weights },
+      { name: "体脂肪", data: body_fats }
+    ]
   end
 
   def new
@@ -35,13 +33,28 @@ class FatsController < ApplicationController
   end
 
   def edit
-    fat = Fat.find(params[:id])
-    body = Body.find(fat[:id])
+    @fat_body = Fat.find(params[:id])
+  end
+
+  def update
+    @fat_body = FatBody.new(fat_params)
+    if @fat_body.valid?
+      @fat_body.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    if user_signed_in?
+      redirect_to fats_path
+    end
   end
 
   private
 
   def fat_params
-    params.require(:fat_body).permit(:weight, :body_fat, :day, :sex_id, :age, :height, :nutrients_p, :nutrients_f, :nutrients_c, :goal_body_fat ).merge(user_id: current_user.id)
+    params.require(:fat_body).permit(:weight, :body_fat, :day, :sex_id, :age, :height, :nutrients_p, :nutrients_f, :nutrients_c, :goal_body_fat, :fat_id ).merge(user_id: current_user.id)
   end
 end
--- app/models/fat_body.rb
+++ app/models/fat_body.rb
@@ -9,6 +9,7 @@ class FatBody
   end
   validates :day, presence: true
   validates :user_id, presence: true
+  validates :fat_id, presence: true
 
   validates :sex_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   with_options format: { with: /\A[0-9]+\z/ } do
@@ -21,11 +22,6 @@ class FatBody
   validates :goal_body_fat, numericality: { greater_than: 0, message: "is invalid"}, format: { with: /\A[0-9]+(\.[0-9]+)?\z/ }
 
   def save
-    fat = Fat.create(weight: weight, body_fat: body_fat, day: day, user_id: user_id)
-    Body.create(sex_id: sex_id, age: age, height: height, nutrients_p: nutrients_p , nutrients_f: nutrients_f, nutrients_c: nutrients_c, goal_body_fat: goal_body_fat, fat_id: fat.id)
-  end
-
-  def update
     fat = Fat.create(weight: weight, body_fat: body_fat, day: day, user_id: user_id)
     Body.create(sex_id: sex_id, age: age, height: height, nutrients_p: nutrients_p , nutrients_f: nutrients_f, nutrients_c: nutrients_c, goal_body_fat: goal_body_fat, fat_id: fat.id)
   end
