eb68e29880a7774872d66b2c8f721c546637c9a3
HSKINK <yuhikwut@gmail.com>
2022-01-07 21:02:54 +0900

    javascriptクリック

--- app/javascript/fat_body.js
+++ app/javascript/fat_body.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('click', function(){
   const pullDownButton = document.getElementById("lists")
   console.log(pullDownButton)
 })
