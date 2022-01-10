d6ec1e2a6a287f4b7a46acb32cb41b36008edfd5
HSKINK <yuhikwut@gmail.com>
2022-01-10 21:25:06 +0900

    javascript

--- app/javascript/fat_body.js
+++ app/javascript/fat_body.js
@@ -9,25 +9,28 @@ window.addEventListener('load', function(){
         gender = radio[i].value;
       }
     }
-    console.log(gender)
-    console.log(dates[2].value)
-    console.log(dates[3].value)
-    console.log(dates[4].value)
-    console.log(dates[5].value)
-    console.log(dates[6].value)
-    console.log(dates[7].value)
-    console.log(dates[8].value)
+    age = parseInt(dates[2].value);
+    height = parseInt(dates[3].value);
+    weight = parseFloat(dates[4].value);
+    body_fat = parseFloat(dates[5].value);
+    nutrients_p = parseFloat(dates[6].value);
+    nutrients_f = parseFloat(dates[7].value);
+    nutrients_c = parseFloat(dates[8].value);
+
+    bmi = (weight * 100 * 100) / (height * height);  
+    if (gender = "male"){
+      tdee = 66.4730 + 13.7516 * weight + 5.0033 * height - 6.7550 * age;
+    }else{
+      tdee = 655.0955 + 9.5634 * weight + 1.8496 * height - 4.6756 * age;
+    }
+    tdee1 = tdee * 1.5;
+    tdee2 = tdee * 1.75;
+    tdee3 = tdee * 2;
+    
+    p = tdee * nutrients_p / 400;
+    f = tdee * nutrients_f / 900;
+    c = tdee * nutrients_c / 400;
+
+    return Number(document.querySelector("#bmi").value);
   })
 })
-// window.addEventListener('load', () => {
-//   const priceInput = document.getElementById("item-price");
-//   priceInput.addEventListener("input", () => {
-//     const inputValue = priceInput.value;
-
-//     const addTaxDom = document.getElementById("add-tax-price");
-//     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
-
-//     const profit = document.getElementById("profit");
-//     profit.innerHTML = inputValue - addTaxDom.innerHTML
-//   })
-// });
