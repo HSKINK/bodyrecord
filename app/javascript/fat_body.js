01c9d7a5d6aa74250c1337f1ae4927e369629d4d
HSKINK <yuhikwut@gmail.com>
2022-01-11 20:17:52 +0900

    javascript

--- app/javascript/fat_body.js
+++ app/javascript/fat_body.js
@@ -18,19 +18,19 @@ window.addEventListener('load', function(){
     nutrients_c = parseFloat(dates[8].value);
 
     bmi = (weight * 100 * 100) / (height * height);  
+    
     if (gender = "male"){
       tdee = 66.4730 + 13.7516 * weight + 5.0033 * height - 6.7550 * age;
     }else{
       tdee = 655.0955 + 9.5634 * weight + 1.8496 * height - 4.6756 * age;
     }
-    tdee1 = tdee * 1.5;
-    tdee2 = tdee * 1.75;
-    tdee3 = tdee * 2;
     
     p = tdee * nutrients_p / 400;
     f = tdee * nutrients_f / 900;
     c = tdee * nutrients_c / 400;
 
-    return Number(document.querySelector("#bmi").value);
+    tdee1 = tdee * 1.5;
+    tdee2 = tdee * 1.75;
+    tdee3 = tdee * 2;
   })
 })
