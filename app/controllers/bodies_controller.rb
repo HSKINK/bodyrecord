de4cfb9ed659fe6626f8e2866a55f662aa1740fb
HSKINK <yuhikwut@gmail.com>
2022-02-26 19:46:06 +0900

    update

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -3,4 +3,16 @@ class BodiesController < ApplicationController
     binding.pry
     @body = Body.find(params[:id])
   end
+
+  def update
+    body = Body.find(params[:id])
+    body.update(body_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def body_params
+    params.require(:fat).permit(:day,:weight, :body_fat).merge(nutrient_id: current_user.body.nutrient.id)
+  end
 end
--- app/controllers/nutrients_controller.rb
+++ app/controllers/nutrients_controller.rb
@@ -3,4 +3,16 @@ class NutrientsController < ApplicationController
     binding.pry
     @nutrient = Nutrient.find(params[:id])
   end
+
+  def update
+    nutrient = Nutrient.find(params[:id])
+    nutrient.update(nutrient_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def nutrient_params
+    params.require(:fat).permit(:day,:weight, :body_fat).merge(nutrient_id: current_user.body.nutrient.id)
+  end
 end
