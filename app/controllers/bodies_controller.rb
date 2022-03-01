caac7d399d95fba8f53446260ca5b5c66189e7ff
HSKINK <yuhikwut@gmail.com>
2022-03-01 19:31:51 +0900

    ,

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -1,6 +1,5 @@
 class BodiesController < ApplicationController
   def edit
-    binding.pry
     @body = Body.find(params[:id])
   end
 
--- app/controllers/nutrients_controller.rb
+++ app/controllers/nutrients_controller.rb
@@ -1,6 +1,5 @@
 class NutrientsController < ApplicationController
   def edit
-    binding.pry
     @nutrient = Nutrient.find(params[:id])
   end
 
