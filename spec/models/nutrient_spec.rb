b84b411321dcc17adee1331f60c59d67fe69ddb9
HSKINK <yuhikwut@gmail.com>
2022-03-18 20:17:53 +0900

    sleep

--- spec/models/nutrient_spec.rb
+++ spec/models/nutrient_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Nutrient, type: :model do
     user = FactoryBot.create(:user)
     body = FactoryBot.create(:body, user_id: user.id)
     @nutrient = FactoryBot.build(:nutrient, body_id: body.id)
+    sleep(1)
     # @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
   end
   describe '3大栄養素バランス' do
