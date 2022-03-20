ab3326dada5b1b017534af5e073ab65cf1261cf7
HSKINK <yuhikwut@gmail.com>
2022-03-20 21:04:08 +0900

    body

--- spec/factories/body.rb
+++ spec/factories/body.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :nutrient do
-    gender_id { '2' }
+  factory :body do
+    gender_id { Faker::Number.between(from: 2, to: 3) }
     age       { '40' }
     tall      { '167' }
     association :user
