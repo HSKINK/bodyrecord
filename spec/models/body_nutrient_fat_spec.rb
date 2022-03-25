2328af3fa042af5b31551c970ff1a5b43fb9db94
HSKINK <yuhikwut@gmail.com>
2022-03-25 23:29:55 +0900

    models

--- spec/models/body_nutrient_fat_spec.rb
+++ spec/models/body_nutrient_fat_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe BodyNutrientFat, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
-      sleep(1)
     end
 
     context '内容に問題ない場合' do
