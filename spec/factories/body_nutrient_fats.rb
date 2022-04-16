c56ea791ab0f68e0cea8d29bb1f129bc5cb3cc77
HSKINK <yuhikwut@gmail.com>
2022-04-16 14:17:00 +0900

    error

--- spec/factories/body_nutrient_fats.rb
+++ spec/factories/body_nutrient_fats.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     n_fat          { '20' }
     n_carbohydrate { '40' }
     goal_body_fat  { '15' }
-    day            { '01/05/2019' }
+    day            { '2019/10/10' }
     weight         { '60' }
     body_fat       { '17' }
   end
