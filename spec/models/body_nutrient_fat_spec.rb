7f3128a6d5c0ffa47632f9b0d67ce8295c1a9ef9
HSKINK <yuhikwut@gmail.com>
2022-05-08 16:02:27 +0900

    binding.pry

--- spec/models/body_nutrient_fat_spec.rb
+++ spec/models/body_nutrient_fat_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BodyNutrientFat, type: :model do
-
+binding.pry
   describe '身体情報新規登録' do
-    binding.pry
     before do
       user = FactoryBot.create(:user)
       @body_nutrient_fat = FactoryBot.build(:body_nutrient_fat, user_id: user.id)
